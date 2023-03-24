7241063505db1fe5609f4d67a83742d96b4d161b
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-03-23 23:58:13 -0400

    Update 'Random Quote Generator' project => index.js file

--- random-quote-generator/index.js
+++ random-quote-generator/index.js
@@ -7,6 +7,12 @@ const apiURL = 'https://api.quotable.io/random';
 async function getQuote() {
 	try {
 		// console.log('clicked');
+		btnEl.innerText = 'Loading...🔋';
+		btnEl.disabled = true;
+
+		quoteEl.innerText = 'Updating...⌛';
+		authorEl.innerText = 'Updating...🪫';
+
 		const response = await fetch(apiURL);
 		const data = await response.json();
 		// console.log(data)
@@ -17,11 +23,19 @@ async function getQuote() {
 
 		quoteEl.innerText = quoteContent;
 		authorEl.innerText = '~ ' + quoteAuthor;
-		// console.log(data);
+
+		btnEl.innerText = 'Get a Quote';
+		btnEl.disabled = false;
 	} catch (error) {
 		console.log(error);
 		quoteEl.innerText = 'An error happened, try again later';
+		authorEl.innerText = 'An error happened';
+
+		btnEl.innerText = 'Get a Quote';
+		btnEl.disabled = false;
 	}
 }
+
+getQuote();
 
 btnEl.addEventListener('click', getQuote);
