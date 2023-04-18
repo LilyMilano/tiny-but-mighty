674c90d2c9bac2e85f6f1ce672ba8b0fd103234e
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-04-17 22:05:34 -0400

    Add 'slot-machine' project

---
+++ slot-machine/project.js
@@ -0,0 +1,43 @@
+// SLOT MACHINE:
+// 1. Deposit some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Give the user their winnings
+// 7. Play again
+
+// 1. Deposit some money
+
+const prompt = require('prompt-sync')();
+
+const deposit = () => {
+	while (true) {
+		const depositAmount = prompt('Enter a deposit amount: ');
+		const numberDepositAmount = parseFloat(depositAmount);
+
+		if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+			console.log('Invalid deposit amount, try again.');
+		} else {
+			return numberDepositAmount;
+		}
+	}
+};
+
+// 2. Determine number of lines to bet on
+
+const getNumberOfLines = () => {
+	while (true) {
+		const lines = prompt('Enter the number of lines to bet on (1-3): ');
+		const numberOfLines = parseFloat(lines);
+
+		if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+			console.log('Invalid number of lines, try again.');
+		} else {
+			return numberOfLines;
+		}
+	}
+};
+
+const depositAmount = deposit();
+const numberOfLines = getNumberOfLines();
